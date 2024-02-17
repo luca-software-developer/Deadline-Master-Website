f8876e640fd60b8dc079694917cecb1bbf6cf8c3
Luca Dello Russo <lucadellorusso5@gmail.com>
2024-02-17 18:05:04 +0100

    Sito Web Deadline Master

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Deadline Master è un'applicazione per la gestione delle scadenze." />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+
+    <title>Deadline Master</title>
+
+    <link rel="apple-touch-icon" sizes="144x144" href="./apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+    <link rel="manifest" href="./site.webmanifest" />
+    <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#da532c" />
+    <link rel="stylesheet" href="./css/style.css" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+</head>
+
+<body id="top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <div class="container px-4">
+            <img class="logo" src="./img/logo.png" alt="Deadline Master" title="Deadline Master" width="144" height="144" />
+            <a class="navbar-brand" href="#top">Deadline Master</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#features"><i class="bi bi-check2-square"></i>
+                            Funzionalità</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#download"><i class="bi bi-download"></i>
+                            Download</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#github"><i class="bi bi-github"></i> Repository
+                            GitHub</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="bg-primary bg-gradient text-white">
+        <div class="container px-4 text-center">
+            <img class="logo" src="./img/logo.png" alt="Deadline Master" title="Deadline Master" width="144" height="144" />
+            <h1 class="fw-bolder">Deadline Master</h1>
+            <p class="lead">Deadline Master è un'applicazione per la gestione delle scadenze.</p>
+            <a class="btn btn-lg btn-light" href="#download"><i class="bi bi-download"></i>&emsp;Download</a>
+            <div id="screenshot-dm">
+                <img src="./img/screenshot-dm.png" alt="Deadline Master" title="Deadline Master" width="1200" height="880" />
+            </div>
+        </div>
+    </header>
+    <section id="features">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2><i class="bi bi-check2-square"></i>&emsp;Funzionalità</h2>
+                    <p class="lead">Deadline Master è un'applicazione basata su Java e JavaFX per gestire scadenze. Non
+                        richiede installazione ed è compatibile con Windows, macOS e Linux.</p>
+                    <ul>
+                        <li>Interfaccia utente semplice e intuitiva;</li>
+                        <li>Compatibilità con tutte le piattaforme che supportano Java 8;</li>
+                        <li>Non richiede installazione e può essere lanciata da una pendrive;</li>
+                        <li>Possibilità di switchare tra diversi file di scadenze senza riavviare l'applicazione;</li>
+                        <li>Funzionalità di modifica avanzate e shortcut da tastiera per risparmiare tempo;</li>
+                        <li>Guida utente integrata.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-light" id="download">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2><i class="bi bi-download"></i>&emsp;Download</h2>
+                    <p class="lead">Scaricare e utilizzare un programma non è mai stato così semplice. L'applicazione
+                        Deadline Master è distribuita come file JAR (Java Archive) avviabile e non richiede
+                        installazione. Per poterla utilizzare è sufficiente disporre di <a target="_blank" href="https://www.oracle.com/it/java/technologies/downloads/#java8">Oracle Java
+                            8</a>.<br /><br />Scarica Deadline Master da <a href="https://github.com/luca-software-developer/Deadline-Master/releases/latest/download/Deadline-Master.jar">qui</a>.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="github">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2><i class="bi bi-github"></i>&emsp;Repository GitHub</h2>
+                    <p class="lead">Puoi trovare il codice sorgente di Deadline Master, tutte le release e ulteriori
+                        informazioni sul <a target="_blank" href="https://github.com/luca-software-developer/Deadline-Master">repository GitHub</a>
+                        ufficiale del progetto.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="py-5 bg-dark">
+        <div class="container px-4">
+            <p class="m-0 text-center text-white">Copyright &copy; Deadline Master <?php echo date('Y'); ?></p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
