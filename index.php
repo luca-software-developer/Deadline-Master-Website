26a73c3f72882ad08a8b370e50ebf470cd62b06a
Luca Dello Russo <lucadellorusso5@gmail.com>
2024-02-21 15:03:57 +0100

    Grafica migliorata

--- index.php
+++ index.php
@@ -2,7 +2,24 @@
 <html lang="it" dir="ltr">
 
 <head>
-    <script type="text/javascript">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Deadline Master è un'applicazione per la gestione delle scadenze.">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Deadline Master</title>
+
+    <link rel="apple-touch-icon" sizes="144x144" href="./apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+    <link rel="manifest" href="./site.webmanifest">
+    <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#da532c">
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <script>
         var _iub = _iub || [];
         _iub.csConfiguration = {
             "askConsentAtCookiePolicyUpdate": true,
@@ -29,42 +46,25 @@
             }
         };
     </script>
-    <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3501513.js"></script>
-    <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
-
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Deadline Master è un'applicazione per la gestione delle scadenze." />
-    <meta name="msapplication-TileColor" content="#da532c" />
-    <meta name="theme-color" content="#ffffff" />
-
-    <title>Deadline Master</title>
-
-    <link rel="apple-touch-icon" sizes="144x144" href="./apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-    <link rel="manifest" href="./site.webmanifest" />
-    <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#da532c" />
-    <link rel="stylesheet" href="./css/style.css" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <script src="https://cs.iubenda.com/autoblocking/3501513.js"></script>
+    <script src="//cdn.iubenda.com/cs/iubenda_cs.js" async></script>
 </head>
 
 <body id="top">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container px-4">
             <a class="navbar-brand" href="#top">
-                <img class="logo" src="./img/logo.png" alt="Deadline Master" title="Deadline Master" width="144" height="144" />
+                <img class="logo" src="./img/logo.png" alt="Deadline Master" title="Deadline Master" width="144" height="144">
                 Deadline Master
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="#features"><i class="bi bi-check2-square"></i>
-                            Funzionalità</a></li>
+                            &ensp;Funzionalità&emsp;</a></li>
                     <li class="nav-item"><a class="nav-link" href="#download"><i class="bi bi-download"></i>
-                            Download</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#github"><i class="bi bi-github"></i> Repository
+                            &ensp;Download&emsp;</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#github"><i class="bi bi-github"></i>&ensp;&nbsp;Repository
                             GitHub</a></li>
                 </ul>
             </div>
@@ -72,12 +72,12 @@
     </nav>
     <header class="bg-primary bg-gradient text-white">
         <div class="container px-4 text-center">
-            <img class="logo" src="./img/logo.png" alt="Deadline Master" title="Deadline Master" width="144" height="144" />
+            <img class="logo" src="./img/logo.png" alt="Deadline Master" title="Deadline Master" width="144" height="144">
             <h1 class="fw-bolder">Deadline Master</h1>
             <p class="lead">Deadline Master è un'applicazione per la gestione delle scadenze.</p>
             <a class="btn btn-lg btn-light" href="#download"><i class="bi bi-download"></i>&emsp;Download</a>
             <div id="screenshot-dm">
-                <img src="./img/screenshot-dm.png" alt="Deadline Master" title="Deadline Master" width="1200" height="880" />
+                <img src="./img/screenshot-dm.png" alt="Deadline Master" title="Deadline Master" width="1200" height="880">
             </div>
         </div>
     </header>
@@ -108,7 +108,7 @@
                     <p class="lead">Scaricare e utilizzare un programma non è mai stato così semplice. L'applicazione
                         Deadline Master è distribuita come file JAR (Java Archive) avviabile e non richiede
                         installazione. Per poterla utilizzare è sufficiente disporre di <a target="_blank" href="https://www.oracle.com/it/java/technologies/downloads/#java8-windows">Oracle Java
-                            8</a>.<br /><br />Scarica l'ultima versione di Deadline Master da <a href="https://github.com/luca-software-developer/Deadline-Master/releases/latest/download/Deadline-Master.jar">qui</a>.
+                            8</a>.<br><br>Scarica l'ultima versione di Deadline Master da <a href="https://github.com/luca-software-developer/Deadline-Master/releases/latest/download/Deadline-Master.jar">qui</a>.
                     </p>
                 </div>
             </div>
@@ -140,7 +140,7 @@
         </p>
         <p class="text-center">
             <a href="https://www.iubenda.com/privacy-policy/68246498" class="iubenda-white no-brand iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">Privacy Policy</a>
-            <script type="text/javascript">
+            <script>
                 (function(w, d) {
                     var loader = function() {
                         var s = d.createElement("script"),
@@ -158,7 +158,7 @@
                 })(window, document);
             </script>
             <a href="https://www.iubenda.com/privacy-policy/68246498/cookie-policy" class="iubenda-white no-brand iubenda-noiframe iubenda-embed iubenda-noiframe " title="Cookie Policy ">Cookie Policy</a>
-            <script type="text/javascript">
+            <script>
                 (function(w, d) {
                     var loader = function() {
                         var s = d.createElement("script"),
@@ -177,8 +177,8 @@
             </script>
         </p>
     </footer>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="./js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
