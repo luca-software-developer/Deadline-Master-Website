dfc9e12e4a9810268803d0abd53217681e2f2d9a
Luca Dello Russo <lucadellorusso5@gmail.com>
2024-02-21 16:11:54 +0100

    Annotazione link rel="canonical"

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 
     <title>Deadline Master</title>
 
+    <link rel="canonical" href="https://deadlinemaster.altervista.org/">
     <link rel="apple-touch-icon" sizes="144x144" href="./apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
